f2f7ed8e8220660296bd7fb7fbf6b97d05753a9b
elf0724 <elf0724@gmail.com>
2026-07-23 16:17:27 +0800

    Update vite.config.js

--- 1999dis/vite.config.js
+++ 1999dis/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // 只需要路徑開頭與結尾的斜線
-  base: '/entrance/1999dis/', 
+  base: '/entrance/', 
   plugins: [react()],
 })
