c8b9c15c93230d5cc02c2cb0aca622aaee69ddb0
elf0724 <elf0724@gmail.com>
2026-07-23 15:52:34 +0800

    Update vite.config.js

--- 1999dis/vite.config.js
+++ 1999dis/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // https://elf0724.github.io/entrance/1999dis/
-  base: 'https://elf0724.github.io/entrance/1999dis/', 
+  base: '/entrance/1999dis/', 
   plugins: [react()],
 })
