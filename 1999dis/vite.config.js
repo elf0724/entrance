e41bce942a588ff202fc19ea14a342a1d935e77a
elf0724 <elf0724@gmail.com>
2026-07-23 15:50:45 +0800

    Add files via upload

---
+++ 1999dis/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  // https://elf0724.github.io/entrance/1999dis/
+  base: 'https://elf0724.github.io/entrance/1999dis/', 
+  plugins: [react()],
+})
